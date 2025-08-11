927e39fa5d4297d553663012c627dddc43730483
dai <twice5814@163.com>
2025-08-11 17:40:51 +0800

    历史消息查询

--- src/main/java/xiaowu/social_network_demo/controller/ChatMessageController.java
+++ src/main/java/xiaowu/social_network_demo/controller/ChatMessageController.java
@@ -1,11 +1,13 @@
 package xiaowu.social_network_demo.controller;
 
+import jakarta.annotation.Resource;
 import lombok.Data;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import xiaowu.social_network_demo.common.Result;
 import xiaowu.social_network_demo.mdoel.ChatMessage;
+import xiaowu.social_network_demo.service.ChatMessageService;
 import xiaowu.social_network_demo.service.GroupMessageService;
 
 @RestController
@@ -14,6 +16,9 @@ import xiaowu.social_network_demo.service.GroupMessageService;
 public class ChatMessageController {
     private final GroupMessageService messageService;
 
+    @Resource
+    private  final ChatMessageService chatMessageService;
+
 
     @PostMapping
     private Result<String> sendGroupMessage(Integer groupId,String content,String UserIp,String sessionId)
@@ -21,7 +26,7 @@ public class ChatMessageController {
         return messageService.sendGroupMessage(groupId,content,UserIp,sessionId);
     }
 
-    public Result<ChatMessage> getConversation(String ip1, String ip2) {
-        return Result.success("获取对话记录成功", messageService.getConversation(ip1, ip2));
+    public Result<ChatMessage> getConversation(Integer groupId) {
+        return Result.success("获取对话记录成功",chatMessageService.getGroupMessages(groupId) );
     }
 }
